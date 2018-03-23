6cb7b40e0e5f028b75c9cabd8173ddc18ba0987c
Luis Montealegre <montealegreluis@gmail.com>
2018-03-23 11:51:20 -0500

    3. Complete exercise 2

--- js/while.js
+++ js/while.js
@@ -26,3 +26,26 @@ do {
 } while (number <= 65536 /* continue condition */);
 
 
+var allCones = Math.floor(Math.random() * 50) + 50; /* initialization */
+do {
+    // thing(s) to repeat
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+
+    if (conesSold >= allCones) { // I have enough cones to sell
+        console.log(conesSold + ' cones sold...');
+        /* increment/decrement/accumulator */
+        allCones -= conesSold;
+    } else { // this is the special case
+        console.log('Cannot sell you ' + conesSold + ' cones I only have ' + allCones + '...');
+    }
+
+} while (allCones > 0);
+console.log('Yay! I sold them all!');
+
+
+
+
+
+
+
+
