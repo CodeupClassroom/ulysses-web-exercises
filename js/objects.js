6b093fb0444545088c93cf3a02e66fc472181919
Luis Montealegre <montealegreluis@gmail.com>
2018-03-27 10:29:16 -0500

    2. Complete the first part of the exercise

--- js/objects.js
+++ js/objects.js
@@ -2,7 +2,6 @@
     "use strict";
 
     /**
-     * TODO:
      * Create an object with firstName and lastName properties that are strings
      * with your first and last name. Store this object in a variable named
      * `person`.
@@ -11,6 +10,18 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
+    // var person = {firstName: "Luis", lastName: "Montealegre"};
+
+    // var person = {};
+    // person.firstName = "Luis";
+    // person.lastName = "Luis";
+
+    var person= {};
+    person["firstName"] = "Luis";
+    person["lastName"] = "Montealegre";
+
+    console.log(person.firstName);
+    console.log(person.lastName);
 
     /**
      * TODO:
